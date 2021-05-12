df9f8cc14ad043b16bac20d8ecffebe0f6fd0654
Josh Winebrener <joshua.winebrener@gmail.com>
2021-05-12 10:19:11 -0500

    more work on the training script

--- training/load.py
+++ training/load.py
@@ -1,13 +1,32 @@
 import os
+import sys
 from glob import glob
-import numpy as np
-from plotly import graph_objects as go
+# import numpy as np
+# from plotly import graph_objects as go
+from scipy.fft import fft
+# from random import randint
 
 import tensorflow as tf
 
 
+DEFAULT_TRAINING_DATA_PATH = '~/Dropbox/belch_o_meter_training_data/'
 NUM_SAMPLES = 220500  # 5 seconds of mono 44.1 kHz audio
 
+def get_training_data_path():
+    path = os.path.expanduser(DEFAULT_TRAINING_DATA_PATH)
+
+    if len(sys.argv) > 1:
+        if isinstance(sys.argv[1], str) and os.path.exists(sys.argv[1]):
+            path = sys.argv[1]
+            if path[-1] != '/':
+                path.append('/')
+        else:
+            print(
+                f'Warning: Invalid path: {sys.argv[1]}\n'
+                + f'Proceeding with {path}...'
+            )
+
+    return path
 
 def get_mono_sample_array(fname):
     # TODO: assert that fname is wav file
@@ -45,18 +64,67 @@ def normalize(samples):
         diff = NUM_SAMPLES - len(samples)
         samples.extend([0] * diff)
 
-    # TODO: Normalize peak volume
-
-    return samples
+    m = float(max(samples))
+    normalized = [n / m for n in samples]
+    return normalized
 
 
 if __name__ == '__main__':
-    bytestreams = {}
+    path = get_training_data_path()
 
-    print('CWD:', os.getcwd())
+    samples = {}
 
-    for fname in glob('assets/*'):
+    files = glob(path + '*.wav')
+    print(os.listdir(path))
+    if not files:
+        raise RuntimeError(f'No .wav files found in {path}')
+
+    for fname in files:
         print('FILE:', fname)
-        bytestreams[fname] = get_mono_sample_array(fname)
+        name = fname.replace(path, '').replace('.wav', '')
+        samples[name] = get_mono_sample_array(fname)
 
-    dataset = tf.data.Dataset.from_tensor_slices(bytestreams)
+    spectra = {}
+
+    for name, sample_array in samples.items():
+        spectrum = abs(fft(sample_array))
+
+        truncated_spectrum = []
+        for i in range(0, len(spectrum), 100):
+            truncated_spectrum.append(
+                sum(spectrum[i:i+100]) / len(spectrum[i:i+100])
+            )
+
+        spectra[name] = truncated_spectrum
+
+    labels = [float(a[0]) for a in spectra.keys()]
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(NUM_SAMPLES),
+        tf.keras.layers.Dense(128),
+        tf.keras.layers.Dense(16),
+        tf.keras.layers.Dense(6),
+    ])
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(
+        optimizer='adam',
+        loss=loss_fn,
+        metrics=['accuracy'],
+    )
+    model.fit(
+        list(spectra.values()),
+        labels,
+        epochs=3,
+    )
+    model.summary()
+
+    print('PREDICTION:', model.predict(list(spectra.values())[:1]))
+    print('ACTUAL:', list(spectra.keys())[:1])
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+
+    # Save the model.
+    with open('model.tflite', 'wb') as f:
+        print('OPENING FILE')
+        f.write(tflite_model)
